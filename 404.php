db2016dcf6c7fcbff778d9fcf82effae6583821b
Dikshant Tanwar <dikshant@cyntexa.com>
2026-02-22 11:01:49 +0530

    Created 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+
+<div style="text-align: center; padding: 100px 20px;">
+    <h1 style="font-size: 80px;">404</h1>
+    <h2>Oops! This destination is off the map.</h2>
+    <p>The travel package or page you are looking for doesn't exist.</p>
+    <br>
+    <a href="index.php" class="btn">Take Me Back Home</a>
+</div>
+
+<?php include 'footer.php'; ?>
